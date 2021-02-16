c944337d64ea68164ff4f2ed30805514c5735b7e
Wylliam Bassos <bassoscolorado@gmail.com>
2021-02-16 09:40:57 -0300

    V1.5
    Remanufature MobileApp

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.25.100:3333',
+  baseURL: 'http://192.168.25.50:3333',
   // baseURL: 'http://localhost:3333',
 });
 
