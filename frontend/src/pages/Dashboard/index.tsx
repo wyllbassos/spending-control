f6eda56eb337c8c4e49d63b9f0df0ebd0f835518
Wylliam Bassos <bassoscolorado@gmail.com>
2021-03-10 00:02:37 -0300

    V1.5.2
    -FrontEnd
    --Change the format of the form!

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Button from '../../components/Button';
 
 import api from '../../services/api';
@@ -24,6 +24,10 @@ const Dashboard: React.FC = () => {
   const [paymentModes, setPaymentModes] = useState<Register[]>([]);
   const [categories, setCategories] = useState<Register[]>([]);
   const [subCategories, setSubCategories] = useState<Register[]>([]);
+  const [state, setState] = useState<{ step: number; params: object[] }>({
+    step: 0,
+    params: [],
+  });
 
   useEffect(() => {
     api.get<Registers>('registers').then(({ data }) => {
@@ -33,6 +37,104 @@ const Dashboard: React.FC = () => {
     });
   }, []);
 
+  const handleNextStep = useCallback((key, value) => {
+    setState(current => ({
+      step: current.step + 1,
+      params: [...current.params, { [key]: value }],
+    }));
+  }, []);
+
+  const handleReset = useCallback(() => {
+    setState(current => ({
+      step: 0,
+      params: [],
+    }));
+  }, []);
+
+  console.log(state);
+
+  return (
+    <>
+      <Header selected="/" />
+      <Container>
+        {state.step === 0 && (
+          <div>
+            <span>Tipo de Transação</span>
+            <Button onClick={() => handleNextStep('type', 'income')}>
+              Entrada
+            </Button>
+            <Button onClick={() => handleNextStep('type', 'outcome')}>
+              Saída
+            </Button>
+          </div>
+        )}
+
+        {state.step === 1 && (
+          <div>
+            <span>Valor</span>
+            <Input type="number" min={0} />
+            <Button onClick={() => handleNextStep('value', 0)}>Avançar</Button>
+          </div>
+        )}
+
+        {state.step === 2 && (
+          <div>
+            <span>Tipo de Pagamento</span>
+            {paymentModes.map(paymentMode => (
+              <Button
+                key={paymentMode.id}
+                onClick={() => handleNextStep('paymentMode', paymentMode.title)}
+              >
+                {paymentMode.title}
+              </Button>
+            ))}
+          </div>
+        )}
+
+        {state.step === 3 && (
+          <div>
+            <span>Categoria</span>
+            {categories.map(category => (
+              <Button
+                key={category.id}
+                onClick={() => handleNextStep('category', category.title)}
+              >
+                {category.title}
+              </Button>
+            ))}
+          </div>
+        )}
+
+        {state.step === 4 && (
+          <div>
+            <span>Sub Categoria</span>
+            {subCategories.map(subCategory => (
+              <Button
+                key={subCategory.id}
+                onClick={() => handleNextStep('subCategory', subCategory.title)}
+              >
+                {subCategory.title}
+              </Button>
+            ))}
+          </div>
+        )}
+
+        {state.step === 5 && (
+          <div>
+            <span>Descrição</span>
+            <Input type="text" />
+            <Button onClick={() => handleNextStep('title', '')}>Avançar</Button>
+          </div>
+        )}
+
+        {state.step !== 0 && (
+          <Button onClick={() => handleReset()}>Voltar</Button>
+        )}
+      </Container>
+    </>
+  );
+
+  /*
   return (
     <>
       <Header selected="/" />
@@ -50,6 +152,15 @@ const Dashboard: React.FC = () => {
         />
 
         <Input type="number" label="Valor" />
+
+        <Select
+          label="Forma de Pagamento"
+          options={[
+            { value: 'outcome', text: 'Crédito' },
+            { value: 'income', text: 'Débito' },
+            { value: 'income', text: 'Dinheiro' },
+          ]}
+        />
 
         <Select
           label="Forma de Pagamento"
@@ -89,6 +200,7 @@ const Dashboard: React.FC = () => {
       </Container>
     </>
   );
+  */
 };
 
 export default Dashboard;
