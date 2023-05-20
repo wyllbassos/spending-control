664857abe2c9f9337ea85806e845dcee3acf5e13
Wylliam Bassos <wylliam@wswork.com.br>
2023-05-19 23:19:47 -0300

    health-check

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,20 @@
+import express, { Router } from 'express';
+import cors from 'cors';
+
 import app from './app';
+
+const app1 = express();
+app.use(cors());
+app.use(express.json());
+app.use(
+  Router().get('/health-check', async (_, response) => {
+    return response.json('OK');
+  }),
+);
+
+app1.listen(10000, () => {
+  console.log('🚀 Server started on port 10000!');
+});
 
 app.listen(443, () => {
   console.log('🚀 Server started on port 443!');
